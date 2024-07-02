fe54019a5a87b76bad6de31f13d068e975836070
AmirHosein <ahtavakkoli96@gmail.com>
2024-07-02 17:32:47 +0330

    creat main file

---
+++ main.py
@@ -0,0 +1,6 @@
+print(':ابعاد جدول را با فاصله وارد کنید')
+n, m = map(int, input().split())
+print(':شکل جدول را وارد کنید')
+puzzle_shape = [input() for i in range(n)]
+print(':کلمات را وارد کنید')
+words=input().split()
