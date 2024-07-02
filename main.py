84dec464b54a08b81f2a921d0e8248d6a3108e76
AmirHosein <ahtavakkoli96@gmail.com>
2024-07-02 18:46:12 +0330

    new commit

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+def compute_length(puzzle_shape):
+    h_lengths=[]
+    v_lengths=[]
+    for i in range(len(puzzle_shape)):
+        for item in puzzle_shape[i]:
+            pass
+    pass
+def crossword_puzzle_solver(puzzle_shape,words):
+    pass
 print(':ابعاد جدول را با فاصله وارد کنید')
 n, m = map(int, input().split())
 print(':شکل جدول را وارد کنید')
