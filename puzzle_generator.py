3fe8826b2e00b216a95f938a4ce2794a617d5402
Ash <shakeriamirhossein1@gmail.com>
2024-07-07 04:31:01 -0700

    finall not really

--- puzzle_generator.py
+++ puzzle_generator.py
@@ -1,33 +1,48 @@
+from itertools import permutations
+from copy import deepcopy
 def checker(word,row,col,puzzel,direction):
-    # فقط حرکت روبه راست و پایین بررسی میشه چونبا بکترکینگ مینویسیم برنمیگردیم درسته ؟
     if direction == "rowy":
         for i in range(len(word)):
             if col + len(word)> len(puzzle[0]):
                 return False
-            if puzzle[row][col+i] == "#":
+            if puzzle[row][col+i] == "_":
+                continue
+            if puzzle[row][col+i] =="#":
                 return False
+            if puzzle[row][col+i] != word[i] :
+                return False
+        
+
     elif direction == "coly":
         for i in range(len(word)):
             if row + len(word) > len(puzzle):
                 return False
-            if puzzel[row+i][col] == "#":
+            if puzzle[row+i][col] == "_":
+                continue
+            if puzzel[row+i][col] == '#':
+                return False
+            if puzzle[row+i][col] != word[i]:
                 return False
     return True
 
-def puzzel_solver(puzzle , name_list , index = 0):
-    if len(name_list) == 0 :
-        puzzle_printer(puzzle)
-        return 
-    word = name_list[index]
-    for direction in ("rowy","coly"):
-        for row in range(len(puzzle)):
-            for col in range(len(puzzle[0])):
-                if checker(word,row,col,puzzle,direction):
-                    place_word(word,row,col,puzzle,direction)
-                    name_list.remove(word)
-                    puzzel_solver(puzzle , name_list , index = 0)
-                    # چجوری ریکرسیو بشه ؟
-                    # چجوری بک ترکینگش کنیم ؟
+def puzzle_solver(puzzle , name_list , index = 0):
+    puzzle_setter = deepcopy(puzzle)
+    for per in permutations(name_list):
+        name_list = list(per)
+        if len(name_list) == index+1:
+            puzzle_printer(puzzle)
+            return 
+        word = name_list[index]
+        for direction in ["rowy","coly"]:
+            for row in range(len(puzzle)):
+                for col in range(len(puzzle[0])):
+                    if checker(word,row,col,puzzle,direction):
+                        #print(word)
+                        #print(name_list)
+                        place_word(word,row,col,puzzle,direction)
+                        puzzle_solver(puzzle , name_list,index = index + 1)
+        puzzle = puzzle_setter
+    
 
 def place_word(word,row,col,puzzle,direction):
     if direction == "rowy" :
@@ -36,12 +51,13 @@ def place_word(word,row,col,puzzle,direction):
     elif direction == "coly" :
         for i in range(len(word)):
             puzzle[row+i][col] = word[i]
-    return puzzle
+    return puzzle,name_list
 
 def puzzle_printer(puzzle):
     for i in range(len(puzzle)):
         print(puzzle[i])
     return
+
 
 # تابع دیگری نیاز داریم ؟
 row,col = list(map(int,input(" pls Enter the row and the column of the puzzle : ").split()))
@@ -54,6 +70,5 @@ for i in range(row):
         mini_puzzle.append(j)
     puzzle.append(mini_puzzle)
     mini_puzzle = []
-name_list = list(input(" Enter the list of the names : ").split()) 
-
-puzzel_solver(puzzle,name_list)
+name_list = list(input(" Enter the list of the names : ").split())
+puzzle_solver(puzzle,name_list)
